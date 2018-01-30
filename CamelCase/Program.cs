b0175a83f0cd6ba3096e79a061a7b42df62c9d73
Phellipe Matos <cphellipematos@outlook.com>
2018-01-30 19:24:04 -0200

    Adicionar arquivos de projeto.

---
+++ CamelCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int result = CamelCase(s);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        protected static int CamelCase(string s)
+        {
+            int count = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() == s[i].ToString().ToUpper())
+                    count++;
+            }
+            return count;
+        }
+    }
+}
